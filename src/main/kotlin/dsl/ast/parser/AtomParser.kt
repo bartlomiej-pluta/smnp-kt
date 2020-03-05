cf0ceba925d873ec031d291dd59a4137bb7da2c1
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-03-05 21:00:40 +0100

    Restore recursive reference to expr parser in atom parser

--- src/main/kotlin/dsl/ast/parser/AtomParser.kt
+++ src/main/kotlin/dsl/ast/parser/AtomParser.kt
@@ -2,11 +2,22 @@ package dsl.ast.parser
 
 import dsl.ast.model.entity.ParserOutput
 import dsl.token.model.entity.TokenList
+import dsl.token.model.enumeration.TokenType
 
 class AtomParser : Parser() {
     override fun tryToParse(input: TokenList): ParserOutput {
+        val parenthesesParser = allOf(
+            listOf(
+                terminal(TokenType.OPEN_PAREN),
+                ExpressionParser(),
+                terminal(TokenType.CLOSE_PAREN)
+            )
+        ) {
+            it[1]
+        }
         val literalParser = oneOf(
             listOf(
+                parenthesesParser,
                 BoolLiteralParser(),
                 FloatLiteralParser(),
                 IntegerLiteralParser(),
