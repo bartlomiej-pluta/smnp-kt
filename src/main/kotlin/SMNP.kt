e58d7450719ac9fd2208b52b18deefdf690fc932
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-03-05 20:48:32 +0100

    Migrate term parser to Kotlin

--- src/main/kotlin/SMNP.kt
+++ src/main/kotlin/SMNP.kt
@@ -2,5 +2,5 @@ import interpreter.Interpreter
 
 fun main(args: Array<String>) {
     val interpreter = Interpreter()
-    interpreter.run("true ** 123 ** 4 -\"fsfsef\".13.15 @c:14 3.14")
+    interpreter.run("2 ** 2 * true ** 123 ** 4 -\"fsfsef\".13.15 @c:14 3.14")
 }
