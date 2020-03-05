e854ac1619d29112fa44f1568fae6090bebd6717
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-03-05 20:38:13 +0100

    Refactor some nodes

--- src/main/kotlin/dsl/ast/model/node/AtomAbstractNode.kt
+++ src/main/kotlin/dsl/ast/model/node/AtomAbstractNode.kt
@@ -3,14 +3,11 @@ package dsl.ast.model.node
 import dsl.token.model.entity.Token
 
 abstract class AtomAbstractNode(token: Token) : Node(1, token.position) {
-    var value: Any
+    val value: Any
     get() = children[0]
-    set(value) {
-        children[0] = value
-    }
 
     init {
-        value = token.value
+        children[0] = token.value
     }
 }
 
--- src/main/kotlin/dsl/ast/model/node/TokenNode.kt
+++ src/main/kotlin/dsl/ast/model/node/TokenNode.kt
@@ -2,14 +2,11 @@ package dsl.ast.model.node
 
 import dsl.token.model.entity.Token
 
-class TokenNode(_token: Token) : Node(1, _token.position) {
-    var type: Any
+class TokenNode(token: Token) : Node(1, token.position) {
+    val token: Any
     get() = children[0]
-    set(value) {
-        children[0] = value
-    }
 
     init {
-        type = _token
+        children[0] = token
     }
 }
