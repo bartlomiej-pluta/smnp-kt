7eb77860e31f5a640d1a95ffb8c465f7d7aa6344
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-03-07 20:11:56 +0100

    Migrate block evaluator to Kotlin

--- src/main/kotlin/io/smnp/evaluation/evaluator/DefaultEvaluator.kt
+++ src/main/kotlin/io/smnp/evaluation/evaluator/DefaultEvaluator.kt
@@ -9,6 +9,7 @@ class DefaultEvaluator : Evaluator() {
 
     override fun tryToEvaluate(node: Node, environment: Environment): EvaluatorOutput {
         return oneOf(
+            BlockEvaluator(),
             ThrowEvaluator(),
             ExpressionEvaluator()
         ).evaluate(node, environment)
