d4577fa4b6cf7247cc8c344e5079059f9ee4ff3a
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-03-25 20:25:44 +0100

    Improve smnp.math and smnp.collection modules

--- modules/math/src/main/kotlin/io/smnp/ext/math/MathModule.kt
+++ modules/math/src/main/kotlin/io/smnp/ext/math/MathModule.kt
@@ -1,16 +1,12 @@
 package io.smnp.ext.math
 
-import io.smnp.ext.provider.HybridModuleProvider
 import io.smnp.ext.math.function.ModuloFunction
 import io.smnp.ext.math.function.RandomFunction
-import io.smnp.ext.math.function.RangeFunction
+import io.smnp.ext.provider.HybridModuleProvider
 import org.pf4j.Extension
 
 @Extension
 class MathModule : HybridModuleProvider("smnp.math") {
-   override fun functions() = listOf(
-      ModuloFunction(), RangeFunction(),
-      RandomFunction()
-   )
+   override fun functions() = listOf(ModuloFunction(), RandomFunction())
    override fun dependencies() = listOf("smnp.lang", "smnp.collection")
 }
