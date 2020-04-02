058b64f3d47b89e4afc74d1a7eefae94f7436e8a
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-04-02 19:40:10 +0200

    Fix smnp.music.tools::alert function

--- modules/music-tools/src/main/kotlin/io/smnp/ext/musictools/MusicToolsModule.kt
+++ modules/music-tools/src/main/kotlin/io/smnp/ext/musictools/MusicToolsModule.kt
@@ -5,5 +5,5 @@ import org.pf4j.Extension
 
 @Extension
 class MusicToolsModule : LanguageModuleProvider("smnp.music.tools") {
-   override fun dependencies() = listOf("smnp.audio.midi", "smnp.music")
+   override fun dependencies() = listOf("smnp.audio.synth", "smnp.audio.midi", "smnp.music")
 }
