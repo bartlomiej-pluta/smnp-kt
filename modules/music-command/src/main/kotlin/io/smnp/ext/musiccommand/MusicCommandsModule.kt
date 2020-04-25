3136a08c632c0c4327d26386527c0ce4621948d8
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-04-25 14:01:57 +0200

    Refactor smnp.music submodules

--- modules/music-command/src/main/kotlin/io/smnp/ext/musiccommand/MusicCommandsModule.kt
+++ modules/music-command/src/main/kotlin/io/smnp/ext/musiccommand/MusicCommandsModule.kt
@@ -1,9 +1,9 @@
-package io.smnp.ext.audio
+package io.smnp.ext.musiccommand
 
 import io.smnp.ext.provider.LanguageModuleProvider
 import org.pf4j.Extension
 
 @Extension
-class MusicCommandsModule : LanguageModuleProvider("smnp.music.commands") {
+class MusicCommandsModule : LanguageModuleProvider("smnp.music.command") {
    override fun dependencies() = listOf("smnp.collection")
 }
